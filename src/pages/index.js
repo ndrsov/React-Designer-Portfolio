a32ee08f1c878ef7bebe7b0b24be09d0ee9db407
Andres Soria <andres.zov@gmail.com>
2023-05-08 10:44:05 -0600

    Set up header navigation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { BsFillMoonStarsFill } from 'react-icons/bs';
 
 export default function Home() {
   return (
@@ -6,7 +7,26 @@ export default function Home() {
       <Head>
         <title>React Designer Portfolio</title>
       </Head>
-      <main></main>
+      <main className='bg-white px-10 '>
+        <section className='min-h-screen'>
+          <nav className='py-10 mb-12 flex justify-between'>
+            <h1 className='text-xl font-burtons'>ANDRES SORIA</h1>
+            <ul className='flex items-center'>
+              <li>
+                <BsFillMoonStarsFill className='cursor-pointer text-2x' />
+              </li>
+              <li>
+                <a
+                  className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8'
+                  href='#'
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </section>
+      </main>
     </div>
   );
 }
