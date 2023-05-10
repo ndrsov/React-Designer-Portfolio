56ed756a5c0e5f7af90222dd35352f6382eb349f
Andres Soria <andres.zov@gmail.com>
2023-05-10 10:55:07 -0600

    Set up avatar image

--- src/pages/index.js
+++ src/pages/index.js
@@ -44,8 +44,14 @@ export default function Home() {
             <AiFillLinkedin />
             <AiFillGithub />
           </div>
-          <div>
-            <Image src={deved} alt='3D portrait' />
+          <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden'>
+            <Image
+              src={deved}
+              alt='3D portrait'
+              priority='true'
+              fill
+              style={{ objectFit: 'cover' }}
+            />
           </div>
         </section>
       </main>
